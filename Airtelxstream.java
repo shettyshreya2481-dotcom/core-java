70556847d2f6de33af9e51e4f2734bd69bbb5e90
shreya-21s <shreya023shetty@gmail.com>
2026-02-05 13:18:05 +0530

    Basic Java Content Regarding on Array

---
+++ Airtelxstream.java
@@ -0,0 +1,10 @@
+class Airtelxstream{
+public static void main(String[] scitificmovies){
+String scienceDocumentary[]={"Cosmos:A Spacetime Odyyssey","Cosmos","Planet Earth","The Blue Planet","The Universe"};
+String astronomySeries[]={"How the Universe Works","Through The Wormhole","Alien Words","Mars","Apollo:Back to the Moon"};
+String futureScience[]={"Black Mirror","Explained","Connected","The Age Of AI","Inside Bill's Brain"};
+String fictionSeries[]={"Dark","Westworld","Stranger Things","Devs","Fringe"};
+
+for(String astronomy:astronomySeries)
+    System.out.println("Astronomy Series:"+astronomy);
+}}
---
+++ Sunnxt.java
@@ -0,0 +1,7 @@
+class Sunnxt{
+public static void main(String[] horror){
+String ghostHorror[]={"The Haunting of Hill House","locke & key","Marriane","Ghost Series"};
+String darkHorror[]={"Dark","The Fell of the House Of Usher","Bates Motel","Servant","Archieve 81"};
+String zombieHorror[]={"The Walking Dead","All of us Are Dead","king dom","Black Summer","Sweet Home"};
+String indianHorror[]={"Ghoul","Betaal","TypeWriter","Bhram","Dahan"};
+}}
