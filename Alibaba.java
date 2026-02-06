84062cfb51b98fe981bf54b9d262ac71b2dca9a6
shettyshreya2481-dotcom <shettyshreya2481@gmail.com>
2026-02-06 12:50:49 +0530

    local Variables

---
+++ Alibaba.java
@@ -0,0 +1,6 @@
+class Alibaba{
+public static void main(String[] Alibaba){
+System.out.println("Create an Account");
+String emailAddress="shetty@gmail.com");
+System.out.println("Email Address:"+emailAddress);
+}}
---
+++ AmazonPrime.java
@@ -0,0 +1,9 @@
+class AmazonPrime{
+public static void main(String[] animation){
+System.out.println("Animation");
+String englishAnime[]={"Rick And Morty","The simpsons","Tom and Jerry"," Avatar The Last Airbender","Bo jack Horseman"};
+String hindiAnime[]={"Chhota Bheem","Motu patlu","Doraemon","Shiva","Little Singham"};
+String adultAnime[]={"South Park","Family Guy","Arcane","Invincible","Castlevania"};
+String japaneseSeries[]={"Demon Slayer","Death Note","Attack on Titan","One Piece","Naruto"};
+}}
+
