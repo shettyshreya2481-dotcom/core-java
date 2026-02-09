61035d8537ced528f21b2fb3c0ef957b4b359c95
shettyshreya2481-dotcom <shettyshreya2481@gmail.com>
2026-02-09 22:16:24 +0530

    deteting unnecessary files

--- Airtelxstream.java
+++
@@ -1,7 +0,0 @@
-class Airtelxstream{
-public static void main(String[] scitificmovies){
-String scienceDocumentary[]={"Cosmos:A Spacetime Odyyssey","Cosmos","Planet Earth","The Blue Planet","The Universe"};
-String astronomySeries[]={"How the Universe Works","Through The Wormhole","Alien Words","Mars","Apollo:Back to the Moon"};
-String futureScience[]={"Black Mirror","Explained","Connected","The Age Of AI","Inside Bill's Brain"};
-String fictionSeries[]={"Dark","Westworld","Stranger Things","Devs","Fringe"};
-}}
